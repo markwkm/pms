4bd39a03788a5dd38074943309334a21f0b420ea
fimath <fimath@24589ed6-a426-0410-b3bc-cf7d0135a5bc>
2007-04-07 19:35:08 +0000

    Enabled the adminitrative interface patch app.
    git-svn-id: https://plm.svn.sourceforge.net/svnroot/plm@151 24589ed6-a426-0410-b3bc-cf7d0135a5bc

--- trunk/plm_dj/patch/models.py
+++ trunk/plm_dj/patch/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Patch(models.Model):
 	diff = models.TextField()
 	name = models.CharField(maxlength=1000)
+
+	class Admin:
+		pass
--- trunk/plm_dj/urls.py
+++ trunk/plm_dj/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     # (r'^plm_dj/', include('plm_dj.foo.urls')),
 
     # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
+     (r'^admin/', include('django.contrib.admin.urls')),
 )
